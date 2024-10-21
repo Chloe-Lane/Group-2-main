44258b15b9a266b0fcda86e47170480c993a41fd
Justin Rieko Tolentino <jstolentino2@student.hau.edu.ph>
2024-10-21 21:50:28 +0800

    try fix

--- codee/settings.py
+++ codee/settings.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
